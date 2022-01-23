66c8ecc330373c26a4b71d353ddf929a54963ab2
PythonT <python.tester12@gmail.com>
2022-01-23 13:37:32 +0100

    try

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 
 ##CREATE DATABASE
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///songs-collection.db"
 # Optional: But it will silence the deprecation warning in the console.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
